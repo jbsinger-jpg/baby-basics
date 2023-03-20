3fcae7b9857ebf9d631aaf4c17dab1ee1abe3223
JACOB SINGER <jacobbradleysinger@gmail.com>
2023-03-20 18:04:19 -0500

    implement voting system for users

--- src/pages/ForumMessagePage.jsx
+++ src/pages/ForumMessagePage.jsx
@@ -80,11 +80,48 @@ function ChatMessage({ message }) {
     // -- the entire message list
     // -- the currently selected message?? 
 
-    const { text, uid, photoURL, voteCount, id } = message;
+    const { text, uid, photoURL, voteCount, id, votedUsers } = message;
     // const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-    // const { data: pageData } = useContext(Context);
+    const { data: pageData } = useContext(Context);
     // const messageRef = await firestore.collection(pageData).doc(id);
     const [messageVoteCount, setMessageVoteCount] = useState(voteCount);
+    const [upVoteButtonIsLoading, setUpVoteButtonIsLoading] = useState(false);
+
+
+    const handleUpVote = async () => {
+        setUpVoteButtonIsLoading(true);
+        const messageRef = await firestore.collection(pageData).doc(id);
+        const messageDoc = await messageRef.get().then(doc => {
+            return doc.data();
+        });
+        console.log(messageDoc);
+        if (messageDoc?.votedUsers && messageDoc?.votedUsers?.length > 0) {
+            // check and see if the current user is in the votedUsers array
+            for (let i = 0; i < messageDoc.votedUsers.length; i++) {
+                if (auth.currentUser.email.toString() === messageDoc.votedUsers[i].toString()) {
+                    console.log("match");
+                    setUpVoteButtonIsLoading(false);
+                    return;
+                }
+            }
+        }
+        else {
+            if (votedUsers)
+                await messageRef.update({
+                    votedUsers: [...votedUsers, auth.currentUser.email],
+                    voteCount: voteCount + 1
+                });
+            else
+                await messageRef.update({
+                    votedUsers: [auth.currentUser.email],
+                    voteCount: voteCount + 1
+                });
+            setMessageVoteCount(messageVoteCount + 1);
+        }
+
+        setUpVoteButtonIsLoading(false);
+    };
+
     return (
         // <div style={{ display: "flex", justifyContent: messageClass === 'sent' ? "flex-start" : 'flex-end', padding: "10px" }}>
         <div style={{ display: "flex", justifyContent: 'flex-start', padding: "10px" }}>
@@ -95,7 +132,8 @@ function ChatMessage({ message }) {
                         icon={<ArrowUpIcon />}
                         variant='outline'
                         colorScheme='teal'
-                        onClick={() => setMessageVoteCount(messageVoteCount + 1)}
+                        onClick={handleUpVote}
+                        isLoading={upVoteButtonIsLoading}
                     // onClick={async () => { await messageRef.update({ voteCount: voteCount + 1 }); }}
                     />
                     <Text>{messageVoteCount}</Text>
