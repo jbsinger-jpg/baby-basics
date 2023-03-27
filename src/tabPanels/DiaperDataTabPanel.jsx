4458c120d458f623d9d27282b4bf8c31958b9065
JACOB SINGER <jacobbradleysinger@gmail.com>
2023-03-26 20:52:08 -0500

    add animation to image

--- src/tabPanels/DiaperDataTabPanel.jsx
+++ src/tabPanels/DiaperDataTabPanel.jsx
@@ -1,6 +1,6 @@
 import { Box, Divider, HStack, Image, SkeletonCircle, SkeletonText, Text, VStack, Stack, Icon, Button } from '@chakra-ui/react';
-import { motion, useAnimation, useCycle } from 'framer-motion';
-import React, { useState } from 'react';
+import { AnimatePresence, motion, useAnimation, useCycle } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
 import { StarIcon } from "@chakra-ui/icons";
 
 function Rating() {
@@ -29,12 +29,19 @@ export default function DiaperDataTabPanel({ diaperData, isDiapersLoading }) {
     const MotionButton = motion(Button);
 
     const [flippedCards, setFlippedCards] = useState(new Array(diaperData?.length).fill(false));
+    const [buttonsPressed, setButtonsPressed] = useState(new Array(diaperData?.length).fill(false));
 
     const handleFlip = (index) => {
         const updatedFlippedCards = [...flippedCards];
         updatedFlippedCards[index] = !updatedFlippedCards[index];
         setFlippedCards(updatedFlippedCards);
-        console.log("Flip at index, ", index);
+    };
+
+    const handleButtonsTrigger = (index) => {
+        const updatedButtonsPressed = new Array(diaperData?.length).fill(false);
+        updatedButtonsPressed[index] = !updatedButtonsPressed[index];
+        setButtonsPressed(updatedButtonsPressed);
+
     };
 
     return (
@@ -52,16 +59,17 @@ export default function DiaperDataTabPanel({ diaperData, isDiapersLoading }) {
                                 {!flippedCards[index] ?
                                     <MotionImage
                                         variant="unstyled"
-                                        whileTap={{
-                                            scale: 0.8,
-                                            borderRadius: "100%",
-                                            rotateY: 180
+                                        initial={buttonsPressed[index] ? { scale: 0, rotate: 180 } : { rotate: 0, scale: 1 }}
+                                        animate={{ rotate: 0, scale: 1 }}
+                                        transition={{
+                                            type: "spring",
+                                            stiffness: 260,
+                                            damping: 20
                                         }}
                                         src={diaper.image}
                                         size="sm"
                                         alt="Alternate Text"
                                         style={{ width: 150, height: 200, resizeMode: 'cover' }}
-                                        onAnimationComplete={() => handleFlip(index)}
                                     />
                                     :
                                     <Box h="300px" alignItems="center" justifyContent="center" display="flex">
@@ -96,6 +104,7 @@ export default function DiaperDataTabPanel({ diaperData, isDiapersLoading }) {
                                                     whileFocus={{ scale: 1.2 }}
                                                     onClick={() => {
                                                         handleFlip(index);
+                                                        handleButtonsTrigger(index);
                                                     }}
                                                 >
                                                     Close
@@ -103,6 +112,7 @@ export default function DiaperDataTabPanel({ diaperData, isDiapersLoading }) {
                                             </HStack>
                                         </VStack>
                                     </Box>
+
                                 }
                                 <VStack spacing="1" width={150} justifyContent="center" alignItems="start">
                                     <Text>Amazon Affiliate</Text>
@@ -122,18 +132,36 @@ export default function DiaperDataTabPanel({ diaperData, isDiapersLoading }) {
                             w="320px"
                         >
                             {!flippedCards[index] &&
-                                <MotionButton
-                                    // When the user uses their mouse
-                                    whileHover={{ scale: 1.2 }}
-                                    // When the user tabs
-                                    whileFocus={{ scale: 1.2 }}
-                                    as="a"
-                                    href={diaper.affiliateLink}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
+                                <HStack
+                                    w="320px"
+                                    justifyContent="space-between"
                                 >
-                                    Buy
-                                </MotionButton>
+                                    <MotionButton
+                                        // When the user uses their mouse
+                                        whileHover={{ scale: 1.2 }}
+                                        // When the user tabs
+                                        whileFocus={{ scale: 1.2 }}
+                                        as="a"
+                                        href={diaper.affiliateLink}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Buy
+                                    </MotionButton>
+                                    <MotionButton
+                                        // When the user uses their mouse
+                                        whileHover={{ scale: 1.2 }}
+                                        // When the user tabs
+                                        whileFocus={{ scale: 1.2 }}
+                                        onClick={() => {
+                                            handleFlip(index);
+                                            handleButtonsTrigger(index);
+                                        }}
+                                    >
+                                        Rate
+                                    </MotionButton>
+                                </HStack>
+
                             }
                         </SkeletonText>
                     </VStack>
